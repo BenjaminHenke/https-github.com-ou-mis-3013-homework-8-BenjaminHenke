a55e90c8d86ebd335d8c1b2fe13fdd3a0cfed779
Ben Henke <Benjamin.J.Henke-1@ou.edu>
2020-04-26 21:28:03 -0500

    ALMOST GOT IT TO WORK

---
+++ Homework8/Homework8/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Homework8
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public GameBoard MyGameBoard = new GameBoard();
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = MyGameBoard;
+        }
+
+        public void Button_Click(object sender, RoutedEventArgs e)
+        {//Updates UI and calls gameBoard update
+            var clickedButton = sender as Button;
+            
+            if (MyGameBoard.currentPlayer == GameBoard.CurrentPlayer.X)
+            {
+                clickedButton.Foreground = (SolidColorBrush)(new BrushConverter().ConvertFrom("#811717"));
+            }
+            else if (MyGameBoard.currentPlayer == GameBoard.CurrentPlayer.O)
+            {
+                clickedButton.Foreground = (SolidColorBrush)(new BrushConverter().ConvertFrom("#126712"));
+            }
+            clickedButton.Background = Brushes.WhiteSmoke;
+            clickedButton.Content = MyGameBoard.currentPlayer;
+            clickedButton.IsHitTestVisible = false;
+
+            MyGameBoard.UpdateBoard(clickedButton.Name);
+        }
+
+        private void Restart_Click(object sender, RoutedEventArgs e)
+        {//Restarts Game
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(MyGrid) - 1; i++) // This loop iterates through all the buttons/tiles in the grid and sets changed properties to default
+            {
+                var child = VisualTreeHelper.GetChild(MyGrid, i) as Button;
+                child.Content = null;
+                child.IsHitTestVisible = true;
+                child.Background = (SolidColorBrush)(new BrushConverter().ConvertFrom("#FFDDDDDD"));
+            }
+            MyGameBoard = new GameBoard();
+            this.DataContext = MyGameBoard;
+        }
+    }
+}
